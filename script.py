9766f13269871b87f43add076b10c4f339d7e8c7
jackwillson642 <jackwillson642@gmail.com>
2021-05-22 20:38:11 +0530

    Notification messages improved and RA changed to ra

--- script.py
+++ script.py
@@ -9,19 +9,18 @@ for i in acad_dirs_list: # Initializing new_dir_list
     new_dir = "DF_" + i
     new_dir_list.append(new_dir)
 
+print()
 for i in new_dir_list: # Copying DF to DF_2011
     des_dir = "./" + i
     shutil.copytree("./DF", des_dir, dirs_exist_ok=True) # dirs_exist_ok=True doesn't break the code if the directories already exist.
+    print(i+" directory updated")
 
-print("\n'DF_version' directories created.")
-
-
+print()
 for i in acad_dirs_list: # Copying contents of Acad_Versions/2011 to /Resources/
     src_dir = "./Acad_Versions/" + i
-    des_dir = "./DF_" + i + "/RA/Resources"
+    des_dir = "./DF_" + i + "/ra/Resources"
     shutil.copytree(src_dir, des_dir, copy_function = shutil.copy, dirs_exist_ok=True)
-
-print("Content from version directories copied to 'DF_version/RA/Resources/")
+    print("Contents of Acad_Versions/"+i+" copied to DF_"+i+"/ra/Resources/")
 
 print()
 for i in new_dir_list:
