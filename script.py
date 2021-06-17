971e65b1f5b98aabbaf9f3c47126998949081910
jackwillson642 <jackwillson642@gmail.com>
2021-06-17 19:30:26 +0530

    Problems with compare functionality fixed

--- script.py
+++ script.py
@@ -9,79 +9,89 @@ acad_dirs_list = os.listdir("./AutoCAD_Versions/")
 
 new_dirs_list = []  # Declaring a list that will contain all the new directories
 
-for i in acad_dirs_list:  # Initializing news_dir_list
-    new_dir = "DF_" + i
+for acad_dir in acad_dirs_list:  # Initializing news_dir_list
+    new_dir = "DF_" + acad_dir
     new_dirs_list.append(new_dir)
 
 
-old_dirs_list = []
-for i in new_dirs_list:
-    old_dirs_list.append(i + "_old")
+old_dirs_list = []  # Declaring a list that will contain all the old directories
+for new_dir in new_dirs_list:  # Initializing old_dirs_list
+    old_dirs_list.append(new_dir + "_old")
 
-print("\n")
-for i in old_dirs_list:
-    shutil.rmtree(os.path.join("./", i), ignore_errors=True)
-    print((i + " deleted\n"))
+# Removing DF_2011_old directories
+print("")
+for old_dir in old_dirs_list:
+    shutil.rmtree(os.path.join("./", old_dir), ignore_errors=True)
+    print((old_dir + " deleted"))
 
-print("\n")
-for i in new_dirs_list:
-    src = i
-    dest = "./" + i + "_old"
-    shutil.copytree(src, dest, dirs_exist_ok=False)
-    print((src + " copied to " + dest + "\n3"))
+# DF_2011 copied to DF_2011_old
+print("")
+for new_dir in new_dirs_list:
+    src = new_dir
+    dest = "./" + new_dir + "_old"
+    shutil.copytree(src, dest, copy_function=shutil.copy2, dirs_exist_ok=False)
+    print((src + " copied to " + dest + ""))
 
-for i in new_dirs_list:  # Copying DF to DF_2011
-    des_dir = "./" + i
+# Copying DF to DF_2011
+for new_dir in new_dirs_list:
+    des_dir = "./" + new_dir
     shutil.copytree("./DF", des_dir, dirs_exist_ok=True)
-    print((i + " directory updated/copied\n"))
+    print((new_dir + " directory updated/copied"))
 
-print("\n")
-for i in acad_dirs_list:  # Copying contents of AutoCAD_Versions/2011 to /Resources/
-    src_dir = "./AutoCAD_Versions/" + i
-    des_dir = "./DF_" + i + "/ra/Resources"
+# Copying contents of AutoCAD_Versions/2011 to DF_2011/ra/Resources/
+print("")
+for acad_dir in acad_dirs_list:
+    src_dir = "./AutoCAD_Versions/" + acad_dir
+    des_dir = "./DF_" + acad_dir + "/ra/Resources"
     shutil.copytree(src_dir, des_dir,
                     copy_function=shutil.copy, dirs_exist_ok=True)
-    print("Contents of AutoCAD_Versions/" + i + " copied to DF_" + i + "/ra/Resources/" + "\n")
+    print("Contents of AutoCAD_Versions/" + acad_dir + " copied to DF_" + acad_dir + "/ra/Resources/" + "")
 
 
 def compare(dir1, dir2):
     from filecmp import dircmp
     global datetime_file_name
-    file = open("./" + i + "/" + datetime_file_name, "a")
+    file = open("./" + new_dir + "/" + datetime_file_name, "a")
 
     global acad_dirs_list
     global new_dirs_list
 
-    file.write("Files added:")
+    # file.write("\n")
     # print("Files added:")
     comparison = dircmp(dir1, dir2, ignore=None)
-    for j in comparison.right_only:
-        if os.path.isdir(j):
-            new_dir1 = dir1 + j
-            new_dir2 = dir2 + j
-            compare(new_dir1, new_dir2)
-        file.write(j + "\n")
-        # print(j + "\n")
+    for i in comparison.right_only:
+        # if os.path.isdir(i):
+        #     new_dir1 = dir1 + i
+        #     new_dir2 = dir2 + i
+        #     compare(new_dir1, new_dir2)
+        file.write("Added: " + dir2 +"/"+ i + "\n")
+        # print(i)
 
-    file.write("Files removed:")
+    # file.write("\n")
     # print("Files removed:")
-    for j in comparison.left_only:
-        if os.path.isdir(j):
-            new_dir1 = dir1 + j
-            new_dir2 = dir2 + j
-            compare(new_dir1, new_dir2)
-        file.write(j + "\n")
-        # print(j + "\n")
+    for i in comparison.left_only:
+        # if os.path.isdir(i):
+        #     new_dir1 = dir1 + i
+        #     new_dir2 = dir2 + i
+        #     compare(new_dir1, new_dir2)
+        file.write("Removed: " + dir2 +"/"+ i + "\n")
+        # print(i)
 
-    file.write("Files modified:")
+    # file.write("\n")
     # print("Files modified:")
-    for j in comparison.diff_files:
-        if os.path.isdir(j):
-            new_dir1 = dir1 + j
-            new_dir2 = dir2 + j
-            compare(new_dir1, new_dir2)
-        file.write(j + "\n")
-        # print(j + "\n")
+    for i in comparison.diff_files:
+        # if os.path.isdir(i):
+        #     new_dir1 = dir1 + i
+        #     new_dir2 = dir2 + i
+        #     compare(new_dir1, new_dir2)
+        file.write("Modified: " + dir2 +"/"+ i + "\n")
+        # print(i)
+
+    for i in comparison.common_dirs:
+        new_dir1 = dir1 + "/" + i
+        new_dir2 = dir2 + "/" + i
+        compare(new_dir1, new_dir2)
+        
 
     file.close()
 
@@ -93,18 +103,17 @@ global datetime_file_name
 datetime_file_name = x.strftime("%d-%m-%y_%H-%M.txt")
 
 # Creating a file with the time stamp as the file name. Doesn't work if the file already exists.
-for i in new_dirs_list:
-    box = open("./" + i + "/" + datetime_file_name, "x")
+for new_dir in new_dirs_list:
+    box = open("./" + new_dir + "/" + datetime_file_name, "x")
     box.close()
 
-print("\n")
-for i in new_dirs_list:
-    shutil.make_archive(i, "zip", i)  # Creating zip file
-    # Notifying creation of zip
-    print((i + " compressed into " + i + ".zip" + "\n"))
-
-    dir_left = i + "_old"
-    dir_right = i
+for new_dir in new_dirs_list:
+    dir_left = new_dir + "_old"
+    dir_right = new_dir
     compare(dir_left, dir_right)
 
-    os.remove("./" + i + "/" + datetime_file_name)  # Removing timestamp file after zip creation
+# print("")
+for new_dir in new_dirs_list:
+    shutil.make_archive(new_dir, "zip", new_dir)  # Creating zip file
+    print((new_dir + " compressed into " + new_dir + ".zip" + ""))  # Notifying creation of zip
+#     os.remove("./" + new_dir + "/" + datetime_file_name)  # Removing timestamp file after zip creation
